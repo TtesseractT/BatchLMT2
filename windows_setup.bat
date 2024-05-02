a69985092927805c543e8de325d995d1428db552
Tess <Sabian.hibbs@gmail.com>
2024-05-02 12:45:50 +0000

    Update windows_setup.bat

--- windows_setup.bat
+++ windows_setup.bat
@@ -17,9 +17,9 @@ if exist "%UserProfile%\Miniconda3\" (
     start /wait "" "%UserProfile%\Downloads\Miniconda3-latest-Windows-x86_64.exe" /InstallationType=JustMe /RegisterPython=1 /S /D=%UserProfile%\Miniconda3
 )
 
-:: Step 3: Properly invoke PowerShell, initialize Conda environment, install the package, and run the script
-echo Opening PowerShell, initializing Conda, installing 'requests', and running script...
-start "" powershell -NoExit -Command "cd '%UserProfile%\Miniconda3'; .\Scripts\activate; conda activate base; pip install requests; python '%~dp0w10_invoke_setup.py'"
+:: Step 3: Properly invoke Miniconda PowerShell, initialize environment, install package, and run script
+echo Opening Miniconda PowerShell Prompt to update environment, install 'requests', and run script...
+start "" powershell -NoExit -ExecutionPolicy ByPass -Command "& {& '%UserProfile%\Miniconda3\shell\condabin\conda-hook.ps1'; conda activate '%UserProfile%\Miniconda3'; pip install requests; python '%~dp0w10_invoke_setup.py'}"
 
 echo Setup completed!
 pause
