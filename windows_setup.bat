b2525e865645f16dcea37f6963de1a36110ed592
Tess <Sabian.hibbs@gmail.com>
2024-05-02 12:30:48 +0000

    c

--- windows_setup.bat
+++ windows_setup.bat
@@ -1,21 +1,25 @@
 @echo off
 cls
-echo Setting up Miniconda...
+echo Checking for existing Miniconda installation...
 
-:: Step 1: Download Miniconda Installer
-echo Downloading Miniconda Installer...
-powershell -command "Invoke-WebRequest -Uri 'https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe' -OutFile '%UserProfile%\Downloads\Miniconda3-latest-Windows-x86_64.exe'"
+:: Check if Miniconda is already installed
+if exist "%UserProfile%\Miniconda3\" (
+    echo Miniconda is already installed.
+) else (
+    echo Miniconda not found, installing...
 
-:: Step 2: Install Miniconda Silently
-echo Installing Miniconda...
-start /wait "" "%UserProfile%\Downloads\Miniconda3-latest-Windows-x86_64.exe" /InstallationType=JustMe /RegisterPython=1 /S /D=%UserProfile%\Miniconda3
+    :: Step 1: Download Miniconda Installer
+    echo Downloading Miniconda Installer...
+    powershell -command "Invoke-WebRequest -Uri 'https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe' -OutFile '%UserProfile%\Downloads\Miniconda3-latest-Windows-x86_64.exe'"
 
-:: Step 3: Update Conda in a new Miniconda PowerShell session
-echo Updating Conda...
-powershell -command "Start-Process 'powershell.exe' -ArgumentList '-NoExit','-Command', 'conda init; conda update conda -y'"
+    :: Step 2: Install Miniconda Silently
+    echo Installing Miniconda...
+    start /wait "" "%UserProfile%\Downloads\Miniconda3-latest-Windows-x86_64.exe" /InstallationType=JustMe /RegisterPython=1 /S /D=%UserProfile%\Miniconda3
+)
 
-:: Optional: Execute a Python script (uncomment and modify the following line as needed)
-powershell -command "& {Start-Process PowerShell -ArgumentList '-NoExit', '-Command', 'conda run -n base python w10_invoke_setup.py'}"
+:: Step 3: Start the Anaconda PowerShell prompt and run the update and Python script
+echo Opening Anaconda PowerShell Prompt to update Conda and run script...
+start "" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Miniconda3 (64-bit)\Anaconda Powershell Prompt (Miniconda3).lnk" powershell -Command "& {conda update conda -y; conda run -n base python %UserProfile%\path\to\your\script\windows_blank_setup.py}"
 
 echo Setup completed!
 pause
